6766e7d6894239b046e4f997819006b8818b23d0
Georgios <ulidin@gmail.com>
2021-10-31 16:51:08 +0100

    swapi first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+// import useFetch from './useFetch';
+import axios from "axios";
+import {
+  Accordion,
+  Alert,
+  Stack,
+  Container,
+  Row,
+  Col,
+  Button,
+} from "react-bootstrap";
+import SwapiHeader from "./components/SwapiHeader";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [pageNext, setPageNext] = useState(0);
+  const [pagePrevious, setPagePrevious] = useState(0);
+  const nameForm = useRef(null);
+
+  useEffect(() => {
+    getData("https://swapi.dev/api/people/?page=1");
+  }, []);
+
+  const getData = (endpoint) => {
+    axios(endpoint)
+      .then((response) => {
+        setIsLoading(false);
+        console.log("RES", response.data);
+
+        if (response.data.results) {
+          setPosts(response.data.results);
+          setPageNext(response.data.next);
+          setPagePrevious(response.data.previous);
+        } else {
+          console.log("An error occurred");
+        }
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log("An error happened", error);
+      });
+  };
+
+  const searchFunc = async () => {
+    const searchUrl = `https://swapi.dev/api/people/?search=${nameForm.current.name.value}`;
+    getData(searchUrl);
+  };
+
+  const postsRender = posts.map((post) => (
+    <Accordion key={post.name}>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>{post.name}</Accordion.Header>
+        <Accordion.Body id="accordionBody">
+          <div className="label">
+            <Alert variant="primary">
+              <p>{`Gender: ${post.gender}`}</p>
+              <p>{`Height: ${post.height}`}</p>
+              <p>{`Mass: ${post.mass}`}</p>
+            </Alert>
+          </div>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  ));
+
+  const content = isLoading ? (
+    <div>Loading...</div>
+  ) : (
+    // <Container className="container">
+    <div>
+      <Row>
+        <Col>
+          <Stack className="col-lg mx-auto">
+            <div>{postsRender.slice(0, 5)}</div>
+          </Stack>
+        </Col>
+        <Col>
+          <Stack className="col-lg mx-auto">
+            <div>{postsRender.slice(5, 10)}</div>
+          </Stack>
+        </Col>
+      </Row>
+    </div>
+    /* </Container> */
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SwapiHeader />
+
+      <Container className="container">
+        <div>
+          <Row>
+            <Col>
+              <Stack className="col-lg mx-auto">
+                <div id="search_bar" className="mx-auto mt-5">
+                  <form ref={nameForm}>
+                    <input name={"name"} placeholder="Search character.." />
+                  </form>
+                </div>
+              </Stack>
+            </Col>
+            <Col>
+              <Stack className="col-lg mx-auto">
+                <div id="search_bar" className="mx-auto mt-5">
+                  <button onClick={() => searchFunc()}>Search</button>
+                </div>
+              </Stack>
+            </Col>
+          </Row>
+        </div>
+
+        {content}
+
+        <div className="my_button">
+          <Row>
+            <Col>
+              <Stack className="col-lg mx-auto">
+                <Button
+                  className="mx-auto mt-5"
+                  id="previousButton"
+                  variant="outline-secondary"
+                  disabled={!pagePrevious}
+                  onClick={() => getData(pagePrevious)}
+                >
+                  {`<<< Previous Page`}
+                </Button>
+              </Stack>
+            </Col>
+            <Col>
+              <Stack className="col-lg mx-auto">
+                <Button
+                  className="mx-auto mt-5"
+                  id="nextButton"
+                  variant="outline-secondary"
+                  disabled={!pageNext}
+                  onClick={() => getData(pageNext)}
+                >
+                  {`Next Page >>>`}
+                </Button>
+              </Stack>
+            </Col>
+          </Row>
+        </div>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SwapiHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div id="title">
+      <h1>SWAPI</h1>
+      <h3>The Star Wars API</h3>
+    </div>
+  );
+}
